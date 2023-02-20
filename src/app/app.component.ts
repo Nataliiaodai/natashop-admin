32b44bea8da25684dd6ed5673e62d1c3764eb0ff
Nataliia Odai <nataliiaodai22@gmail.com>
2023-02-20 13:02:57 +0200

    http post

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'natashop-admin';
+
+  constructor(private http: HttpClient) {}
+
+  onProductCreate(products: {prodName: string, prodPrice: string, prodComment: string}) {
+    console.log(products);
+
+    this.http.post('192.168.0.101:3000/products', products)
+      .subscribe((res) => {
+        console.log(res);
+      });
+  }
 }
+
+
+// {
+//   "name": {
+//   "ru" : "My product 2"
+// },
+//   "price": "0,33"
+// }
