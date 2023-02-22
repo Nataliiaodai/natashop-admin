00f7bb12add1e11b8992c9b996b7fe1097a470bd
Nataliia Odai <nataliiaodai22@gmail.com>
2023-02-22 11:46:48 +0200

    correction in post request obj

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,36 +15,65 @@ export class AppComponent  implements OnInit {
   }
 
   ngOnInit() {
-    this.fetchProduct();
+    // this.fetchProduct();
   }
 
-  onProductCreate(products: { prodName: string, prodPrice: string, prodComment: string }) {
-    console.log(products);
 
+    // this.fetchProduct()
+
+
+  onProductCreate(products: {price: string, note: string }) {
     this.http.post('http://localhost:3000/products', products)
       .subscribe((res) => {
-        console.log(res);
+        console.log( res);
       });
+
   }
 
 
   private fetchProduct() {
-    this.http.get('http://localhost:3000/products')
-      .pipe(map((res) => {
-        const products = [];
-        for (const key in res) {
-          if (res.hasOwnProperty(key)) {
-            products.push({...res[key], id: key});
-          }
-        }
-        return products;
-      }))
+    this.http.get('http://localhost:3000/products/4720')
+      // .pipe(map((res) => {
+      //   const products = [];
+      //   for (const key in res) {
+      //     if (res.hasOwnProperty(key)) {
+      //       products.push({...res[key]});
+      //     }
+      //     products.push({...res});
+      //   }
+      //   return products;
+      // }))
       .subscribe((res) => {
         console.log(res);
+        // const keys = res.id.keys();
+        // this.id = keys.map(key =>
+        //   `${key}: ${res.headers.get(key)}`);
       })
   }
 
-
+//   const myList = document.querySelector('ul');
+//   const myRequest = new Request('products.json');
+//
+//   fetch(myRequest)
+// .then((response) => response.json())
+// .then((data) => {
+//   for (const product of data.products) {
+//   const listItem = document.createElement('li');
+//   listItem.appendChild(
+//     document.createElement('strong')
+// ).textContent = product.Name;
+//   listItem.append(
+// ` can be found in ${
+//   product.Location
+// }. Cost: `
+// );
+// listItem.appendChild(
+// document.createElement('strong')
+// ).textContent = `£${product.Price}`;
+// myList.appendChild(listItem);
+// }
+// })
+// .catch(console.error);
 
 
 
