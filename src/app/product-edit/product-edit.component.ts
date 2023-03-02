20dace364d87046f714a32deb84e00fdd7a53329
Nataliia Odai <nataliiaodai22@gmail.com>
2023-03-02 11:44:23 +0200

    tmp

--- src/app/product-edit/product-edit.component.ts
+++ src/app/product-edit/product-edit.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Product} from "../shared-model/product.model";
 import {HttpClient} from "@angular/common/http";
 import {ProductEditService} from "./product-edit.service";
@@ -10,72 +10,72 @@ import {ActivatedRoute, Router} from "@angular/router";
   styleUrls: ['./product-edit.component.css']
 })
 export class ProductEditComponent implements OnInit{
-  prod : Product = new Product();
+  prod: Product = new Product();
   showId = true;
   id: number;
+
 
 
   constructor(private http: HttpClient,
               private productEditService: ProductEditService,
               private router: Router,
-              private route: ActivatedRoute) {}
+              private route: ActivatedRoute) {
+  }
+
 
 
   ngOnInit() {
-    let idToGetProduct = this.route.snapshot.params ['id'];
-    console.log('onInit ' + this.route.snapshot.params ['id']);
-    this.productEditService.getProduct(idToGetProduct)
-      .subscribe((response) => this.prod = response);
+    // this.currentURL = this.router.url;
+    // console.log(this.currentURL);
+    // let idToGetProduct = this.route.snapshot.params ['id'];
+    // console.log('onInit ' + this.route.snapshot.params ['id']);
+    // this.productEditService.getProduct(idToGetProduct)
+    //   .subscribe((response) => this.prod = response);
   }
 
-  // onProductCreate():void  {
-  //   this.showId = true;
-  //   this.productService.createProduct(this.prod).subscribe(
-  //     (response) => this.prod = response,
-  //     (error: any) => console.log(error),
-  //     () => console.log('Done creating shared-model'),
-  //   )
-  // }
 
-  // this.prod = response
-
-  onProductCreate():void  {
+  onProductCreate(): void {
     this.showId = true;
     this.productEditService.createProduct(this.prod).subscribe(
       (response) => {
-        this.id = response._id;
-        this.router.navigate(['admin/product/edit', this.id])
-          .then((data) => console.log(this.router));
+        this.router.navigate(['admin/product/edit', response._id])
+          .then(() => console.log(this.router));
+        this.prod = response;
+        console.log(response);
       },
 
+      (error: any) => console.log(error),
+      () => console.log('Done creating product'),
     )
   }
 
 
-  onProductUpdate():void  {
+  onProductUpdate(): void {
     this.showId = true;
     this.productEditService.updateProduct(this.prod).subscribe(
       (response) => this.prod = response,
       (error: any) => console.log(error),
-      () => console.log('Done updating shared-model'),
+      () => console.log('Done updating product'),
     )
   }
-
 
 
   onProductReset() {
     this.showId = true;
     this.prod = new Product();
+    // this.router.navigate(['admin/product/add'])
+    //   .then(() => console.log('Information  was cleaned'));
   }
 
-  onProductDelete():void  {
+  onProductDelete(): void {
     this.showId = false;
     this.productEditService.deleteProduct(this.prod).subscribe(
       () => this.onProductReset(),
       (error: any) => console.log(error),
-      () => console.log('Done deleting shared-model'),
+      () => console.log('Done deleting product'),
     )
   }
+
 
 }
 
